529ed89c5ee05efcb9fd0c3268918c75db7ae748
remila.antony <remila.antony@zohocorp.com>
2020-12-13 18:44:18 +0530

    View controller - file clean

--- Test/AppDelegate.swift
+++ Test/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let viewController  =   SecondViewController()
+        let viewController  =   ThirdViewController()
         let navController   =   UINavigationController(rootViewController: viewController)
         
         window?.rootViewController  =   navController
---
+++ Test/showOnMap.swift
@@ -0,0 +1,25 @@
+//
+//  EditAlarm.swift
+//  Test
+//
+//  Created by Antony Michale Remila on 14/03/17.
+//  Copyright © 2017 Antony Michale Remila. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import EventKit
+
+
+class ShowOnMap: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor   =   UIColor.init(red: 250/255, green: 250/255, blue: 250/255, alpha: 1.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
